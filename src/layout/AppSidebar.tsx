2f72a2942e8af5a2b51a5bb5657cb9e8b5e308ec
pugarchdev <pugarchdev@gmail.com>
2025-02-19 17:40:53 +0530

    Side menu content change

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -29,7 +29,7 @@ const navItems: NavItem[] = [
   {
     icon: <GridIcon />,
     name: "Dashboard",
-    subItems: [{ name: "Ecommerce", path: "/", pro: false }],
+    subItems: [{ name: "People", path: "/", pro: false }],
   },
   {
     icon: <CalenderIcon />,
