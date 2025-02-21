9128d083205e8cf986da06ddbfac8aa513e66996
pairavat <pairavat@gmail.com>
2025-02-21 15:40:52 +0530

    Main dashboard changes

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -5,6 +5,7 @@ import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
+import SalesPieChart from "../../components/ecommerce/SalesPieChart";
 
 export default function Ecommerce() {
   return (
@@ -14,27 +15,33 @@ export default function Ecommerce() {
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
+        <div className="col-span-12 space-y-6 xl:col-span-12">
           <EcommerceMetrics />
+        </div>
 
+        <div className="col-span-4">
+          <SalesPieChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-4">
           <MonthlySalesChart />
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
+        <div className="col-span-12 xl:col-span-4">
           <MonthlyTarget />
         </div>
 
-        <div className="col-span-12">
+        <div className="col-span-6">
           <StatisticsChart />
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
+        <div className="col-span-6 xl:col-span-6">
           <DemographicCard />
         </div>
 
-        <div className="col-span-12 xl:col-span-7">
+        {/* <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
-        </div>
+        </div> */}
       </div>
     </>
   );
